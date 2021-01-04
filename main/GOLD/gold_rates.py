fe5489d7b58cb3a809d23408e6193851f0423a5f
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2021-01-04 22:10:25 +0530

    Create gold_rates.py

---
+++ main/GOLD/gold_rates.py
@@ -0,0 +1,45 @@
+import http.client
+import json
+import time
+
+
+
+API = input("BOT: ENTER YOUR OPEN-WEATHER-API TOKEN :--- \t")
+print("\n")
+
+
+
+# API-ACCESS FUNCTION
+def gold_api(API):
+
+  payload = ''
+  headers = {
+    'x-access-token': API,
+    'Content-Type': 'application/json'
+  }
+  
+  url = "www.goldapi.io"
+  
+  try:
+      conn = http.client.HTTPSConnection(url)
+      print("LOADING...")
+      time.sleep(2)
+      conn.request("GET", "/api/XAU/INR", payload, headers)
+      res = conn.getresponse()
+      return (json.loads(res.read().decode("utf-8")))
+      
+  except:
+      print("\nERROR!! Try checking the API-PIN, AND INTERNET CONNECTIVITY!")
+
+
+
+# DRIVER CODE..
+try: 
+    data = gold_api(API)
+    gold_price = data['price']
+    print("GOLD PRICE, for today [INR] :\t",gold_price)
+except:
+    print("~Thank You")
+
+
+@ CODED BY TSG405, 2021        
